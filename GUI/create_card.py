be200bb88ccce7281861d39767ade739e1189a95
trmttm <96355189+trmttm@users.noreply.github.com>
2022-05-31 13:18:25 +0400

    GUI update

--- GUI/create_card.py
+++ GUI/create_card.py
@@ -26,54 +26,74 @@ def get_actions(stacker: Stacker):
 
 def get_actions_tree(stacker: Stacker):
     return stacker.vstack(
-        w.Label('lbl_actions').text('Actions'),
-        w.TreeView('tree_actions'),
-        w.Spacer().adjust(-1),
+        w.Label('lbl_actions').text('Actions').padding(10, 0),
+        w.TreeView('tree_actions').padding(10, 10),
+        stacker.hstack(
+            w.Button('btn_remove_action').text('X').padding(0, 10),
+            w.Button('btn_add_action').text('+').padding(0, 10),
+            w.Button('btn_move_action_up').text('↑').padding(0, 10),
+            w.Button('btn_move_action_down').text('↓').padding(0, 10),
+            w.Spacer().adjust(-4),
+            w.Spacer().adjust(-4),
+            w.Spacer().adjust(-4),
+            w.Spacer().adjust(-4),
+        ),
+        w.Spacer().adjust(-2),
     )
 
 
 def get_actions_properties(stacker: Stacker):
-    return w.NoteBook('notebook_actions', stacker).frame_names(('i', 'Files', 'Feedback')).stackers(
-        get_tab_one(stacker),
-        w.TreeView('tree_action_files'),
-        w.Label('lbl_notebook_3').text('3'),
+    return stacker.vstack(
+        w.Label('lbl_actions_spacer'),
+        stacker.hstack(
+            w.Label('lbl_action_name').text('Name').width(10).padding(5, 0),
+            w.Entry('entry_action_name').default_value('Action Name', ).padding(25, 0).width(40),
+            w.Spacer().adjust(-1),
+        ),
+        stacker.hstack(
+            w.Label('lbl_owner').text('Owner').width(10).padding(5, 0),
+            stacker.hstack(
+                w.Entry('entry_owner').default_value('Owner Name').padding(25, 0),
+                w.Label('lbl_importance').text('Importance'),
+                w.Entry('entry_importance').default_value(5).padding(25, 0),
+                w.Spacer().adjust(-1),
+            ),
+            w.Spacer().adjust(-1),
+        ),
+        stacker.hstack(
+            w.Label('lbl_time').text('Time expected').width(10).padding(5, 0),
+            stacker.hstack(
+                w.Entry('entry_time').default_value('1:00').padding(25, 0),
+                w.Label('lbl_done').text('Done'),
+                w.CheckButton('check_done').value(False).padding(25, 0),
+                w.Spacer().adjust(-2),
+            ),
+            w.Spacer().adjust(-1),
+        ),
+        w.NoteBook('notebook_actions', stacker).frame_names(('Detail', 'Files', 'Feedback')).stackers(
+            w.TextBox('text_description').padding(5, 5),
+            stacker.vstack(
+                w.TreeView('tree_action_files'),
+            ),
+            w.Label('lbl_notebook_3').text('3'),
+        ),
+        w.Spacer().adjust(-1)
     )
 
 
 def get_card_property_entry(stacker: Stacker):
     return stacker.vstack(
         stacker.hstack(
-            w.Label('lbl_name').text('Name').width(10),
+            w.Label('lbl_name').text('Name').width(10).padding(10, 0),
             w.Entry('entry_name').default_value('New Card Name', ).padding(25, 0),
             w.Spacer().adjust(-1),
         ),
         stacker.hstack(
-            w.Label('lbl_dead_line').text('Deadline').width(10),
+            w.Label('lbl_dead_line').text('Deadline').width(10).padding(10, 0),
             w.Entry('entry_dead_line').default_value('2022/5/30', ).padding(25, 0),
-            w.Spacer().adjust(-1),
+            w.Spacer(),
+            w.Label('lbl_date_created1').text('Created: ').width(10).padding(10, 0),
+            w.Label('lbl_date_created2').text('2022/5/28').width(10).padding(10, 0),
         ),
         w.Spacer(),
-    )
-
-
-def get_tab_one(stacker: Stacker):
-    return stacker.vstack(
-        w.Label('lbl_description').text('Description').padding(5, 5),
-        w.TextBox('text_description').padding(5, 5),
-        stacker.hstack(
-            w.Label('lbl_time').text('Time').width(10).padding(5, 5),
-            w.Entry('entry_time').default_value('1:00').padding(5, 5),
-            w.Spacer().adjust(-1),
-        ),
-        stacker.hstack(
-            w.Label('lbl_importance').text('Importance').width(10).padding(5, 5),
-            w.Entry('entry_importance').default_value(5).padding(5, 5),
-            w.Spacer().adjust(-1),
-        ),
-        stacker.hstack(
-            w.Label('lbl_done').text('Done').width(10).padding(5, 5),
-            w.CheckButton('check_done').value(False).padding(5, 5),
-            w.Spacer().adjust(-1),
-        ),
-        w.Spacer().adjust(-1),
     )
--- Tests/tests.py
+++ Tests/tests.py
@@ -1,20 +1,39 @@
 import unittest
 
 
+def load_gui(module, parent: str = 'root'):
+    view_model = module.get_view_model(parent)
+    from view_tkinter import View
+    view = View()
+    view.add_widgets(view_model)
+    view.launch_app()
+
+
 class MyTestCase(unittest.TestCase):
     def test_list_of_balls(self):
         import GUI
-        view_model = GUI.list_of_balls.get_view_model('root')
-
-        from view_tkinter import View
-        view = View()
-        view.add_widgets(view_model)
-        view.launch_app()
+        module = GUI.list_of_balls
+        load_gui(module)
 
     def test_gui_create_card(self):
         import GUI
-        view_model = GUI.create_card.get_view_model('root')
+        module = GUI.create_card
+        load_gui(module)
 
+    def test_combined(self):
+        import GUI
+        from stacker import Stacker
+        from stacker import widgets as w
+        stacker = Stacker()
+        stacker.vstack(
+            w.PanedWindow('pw_main', stacker).is_vertical().stackers(
+                w.Label('1').text(''),
+                w.Label('2').text('')
+            )
+        )
+        view_model = stacker.view_model
+        view_model += GUI.list_of_balls.get_view_model('frame_1')
+        view_model += GUI.create_card.get_view_model('frame_2')
         from view_tkinter import View
         view = View()
         view.add_widgets(view_model)
