ddbb7513d5becc47a4e207b0ef144d119a520b97
trmttm <96355189+trmttm@users.noreply.github.com>
2022-06-04 10:49:43 +0400

    if card is not None:

--- Interactor/set_card_name.py
+++ Interactor/set_card_name.py
@@ -5,5 +5,6 @@ from .present_card_list import present_card_list
 
 def execute(e: EntitiesABC, p: PresentersABC, card_name: str):
     card = e.active_card
-    card.set_name(card_name)
-    present_card_list(e, p)
+    if card is not None:
+        card.set_name(card_name)
+        present_card_list(e, p)
--- Interactor/set_dead_line.py
+++ Interactor/set_dead_line.py
@@ -5,5 +5,6 @@ from .present_card_list import present_card_list
 
 def execute(e: EntitiesABC, p: PresentersABC, dead_line_str: str):
     card = e.active_card
-    card.set_dead_line_by_str(dead_line_str)
-    present_card_list(e, p)
+    if card is not None:
+        card.set_dead_line_by_str(dead_line_str)
+        present_card_list(e, p)
