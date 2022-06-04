264af81c062a62ed4dd40df55635d651213ac313
trmttm <96355189+trmttm@users.noreply.github.com>
2022-06-04 09:20:33 +0400

    organizing main

--- Tests/test_app.py
+++ Tests/test_app.py
@@ -3,25 +3,25 @@ import unittest
 
 class MyTestCase(unittest.TestCase):
     def test_add_card(self):
+        gui_selected = 'gui.fmide_gui'
+
         from view_tkinter import View
         from Entities import Entities
         from Interactor import Interactor
         from Presenters import Presenters
         from Gateway import Gateway
-        from Controller import configure_controller
-
-        gui_selected = 'gui.fmide_gui'
+        import Controller as controller
 
         app = View()
         entities = Entities()
         presenters = Presenters(app)
         gateway = Gateway()
         interactor = Interactor(entities, presenters, gateway)
+
+        # Configuration
         interactor.load_gui(gui_selected)
         presenters.set_up_after_gui()
-
-        configure_controller(app, interactor)
-
+        controller.configure_controller(app, interactor)
         app.launch_app()
 
 
