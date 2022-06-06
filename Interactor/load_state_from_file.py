b81f94c257852f359f73f0485a815d2d0580d454
trmttm <96355189+trmttm@users.noreply.github.com>
2022-06-06 20:44:35 +0400

    Delete load_state_from_file.py

--- Interactor/load_state_from_file.py
+++
@@ -1,9 +0,0 @@
-from Entities import EntitiesABC
-from Gateway import GatewayABC
-from Presenters import PresentersABC
-
-
-def execute(e: EntitiesABC, p: PresentersABC, gateway: GatewayABC, file_name: str):
-    state = gateway.load_file(file_name)
-    e.load_state(state)
-    p.load_state(state)
