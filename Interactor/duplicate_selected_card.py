97811a4a077adcc13007cb7aac968a83d98ae910
trmttm <96355189+trmttm@users.noreply.github.com>
2022-06-10 06:40:22 +0400

    Refactoring

--- Interactor/duplicate_selected_card.py
+++ Interactor/duplicate_selected_card.py
@@ -2,12 +2,12 @@ from Commands import DuplicateCard
 from Entities import Card
 from Entities import EntitiesABC
 from Presenters import PresentersABC
-from .present_my_card_list import present_my_card_list
+
+from .present_card_list import present_card_list
 
 
 def execute(e: EntitiesABC, p: PresentersABC, original_card: Card):
     command = DuplicateCard(e, original_card)
     command.execute()
-    next_selection_indexes = (len(e.my_cards) - 1,)
-
-    present_my_card_list(e, p, next_selection_indexes)
+    present_card_list(e, p)
+    e.set_show_this_card(e.active_card)
--- Interactor/load_state_from_file.py
+++ Interactor/load_state_from_file.py
@@ -2,13 +2,11 @@ from Entities import EntitiesABC
 from Gateway import GatewayABC
 from Presenters import PresentersABC
 from .present_action_list import present_action_list
-from .present_my_card_list import present_my_card_list
-from .present_their_card_list import present_their_card_list
+from .present_card_list import present_card_list
 
 
 def execute(e: EntitiesABC, g: GatewayABC, p: PresentersABC, file_name: str):
     state = g.load_file(file_name)
     e.load_state(state)
-    present_my_card_list(e, p, (0,))
-    present_their_card_list(e, p, (0,))
+    present_card_list(e, p)
     present_action_list(e, p, (0,))
