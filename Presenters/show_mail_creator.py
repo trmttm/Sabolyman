dc7323a19a6e0ea0cadfbcd517d4341818e38d24
trmttm <96355189+trmttm@users.noreply.github.com>
2022-06-17 23:19:49 +0400

    Added more EmailTemplates

--- Presenters/show_mail_creator.py
+++ Presenters/show_mail_creator.py
@@ -3,12 +3,18 @@ from stacker import Stacker
 from stacker import widgets as w
 from view_tkinter.tk_interface import widget_model
 
+specified_parent = 'popup_mail'
+text_box_mail_body_id = 'text_mail_body'
+text_box_mail_id = 'text_mail'
+
 
 def execute(v: ViewABC, text: str):
-    specified_parent = 'popup_mail'
-    text_box_mail_body_id = 'text_mail_body'
-    text_box_mail_id = 'text_mail'
+    view_model = create_view_model()
+    v.add_widgets(view_model)
+    v.bind_command_to_widget(text_box_mail_body_id, lambda *_: update_text_mail(text, v))
 
+
+def create_view_model():
     stacker = Stacker(specified_parent)
     stacker.vstack(
         w.Label('lbl_body').text('Body:'),
@@ -17,11 +23,10 @@ def execute(v: ViewABC, text: str):
     )
     view_model = [widget_model('root', specified_parent, 'toplevel', 0, 0, 0, 0, 'nswe', )]
     view_model += stacker.view_model
-    v.add_widgets(view_model)
+    return view_model
 
-    def update_text_mail(current_text: str):
-        body_text = v.get_value(text_box_mail_body_id)
-        text = current_text.replace('[body]', body_text)
-        v.set_value(text_box_mail_id, text)
 
-    v.bind_command_to_widget(text_box_mail_body_id, lambda *_: update_text_mail(text))
+def update_text_mail(current_text: str, v: ViewABC):
+    body_text = v.get_value(text_box_mail_body_id)
+    text = current_text.replace('[body]', body_text)
+    v.set_value(text_box_mail_id, text)
