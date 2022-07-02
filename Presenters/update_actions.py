b6f8d5ae1f5c78c93f16cedd1cc9caa1297e8d8d
trmttm <96355189+trmttm@users.noreply.github.com>
2022-07-02 20:48:36 +0400

    Numbering tree items

--- Presenters/update_actions.py
+++ Presenters/update_actions.py
@@ -15,16 +15,16 @@ def execute(v: ViewABC, second_column_data: tuple, names: Tuple[str, ...], selec
 
 
 def create_view_model(second_column_data: tuple, names: Tuple[str, ...], select_indexes: Tuple[int, ...], **kwargs):
-    headings = 'Name', 'Completed'
-    widths = 100, 130
+    headings = 'No', 'Name', 'Completed'
+    widths = 40, 100, 130
     tree_datas = []
     states: Tuple[bool, ...] = kwargs.get('states', tuple(False for _ in names))
     for n, (name, time_expected, state) in enumerate(zip(names, second_column_data, states)):
         time_expected_str = time_delta_to_str(time_expected)
         tree_datas.append(
-            create_tree_data('', f'{n}', '', (name, time_expected_str), (), n in select_indexes, strikethrough=state)
+            create_tree_data('', f'{n}', '', (n, name, time_expected_str), (), n in select_indexes, strikethrough=state)
         )
-    stretches = True, False
+    stretches = False, True, False
     scroll_v = True
     scroll_h = True
     view_model = create_view_model_tree(headings, widths, tree_datas, stretches, scroll_v, scroll_h)
--- Presenters/update_cards_list.py
+++ Presenters/update_cards_list.py
@@ -14,16 +14,16 @@ def update_cards_list(v: ViewABC, names: tuple, due_dates: tuple, select_indexes
 
 
 def create_view_model(due_dates: tuple, names: tuple, select_indexes: Tuple[int, ...], **kwargs):
-    headings = 'Name', 'Due Date'
-    widths = 100, 130
+    headings = 'No', 'Name', 'Due Date'
+    widths = 40, 100, 130
     tree_datas = []
     all_status = kwargs.get('completions_status', tuple(False for _ in names))
     colors = kwargs.get('colors', tuple(False for _ in names))
     for n, (name, due_date, status, color) in enumerate(zip(names, due_dates, all_status, colors)):
         due_date_str = datetime_to_str(due_date)
-        tree_datas.append(create_tree_data('', f'{n}', '', (name, due_date_str), (), n in select_indexes,
+        tree_datas.append(create_tree_data('', f'{n}', '', (n, name, due_date_str), (), n in select_indexes,
                                            strikethrough=status, background=color))
-    stretches = True, False
+    stretches = False, True, False
     scroll_v = True
     scroll_h = True
     view_model = create_view_model_tree(headings, widths, tree_datas, stretches, scroll_v, scroll_h)
