b641025823178c7ec1c1fa2561bca77f83a51f79
trmttm <96355189+trmttm@users.noreply.github.com>
2022-07-07 06:16:46 +0400

    Update menu_bar.py

--- Controller/menu_bar.py
+++ Controller/menu_bar.py
@@ -13,6 +13,7 @@ def configure_menu_bar(v: ViewABC, i: InteractorABC, e: EntitiesABC, menu_inject
     menu_bar_model = {
         'File': {
             'Save Sate': lambda: i.save_to_file(v.select_save_file(initialfile=utilities.default_file_name(e))),
+            'Save Sate as': lambda: i.save_to_file(v.select_save_file(initialfile=utilities.default_file_name(e))),
             'Load State': lambda: i.load_state_from_file(v.select_open_file()),
         },
         'Export': {
