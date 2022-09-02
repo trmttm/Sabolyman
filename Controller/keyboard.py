442d3621c054db5549f964aae9f703bb6160c341
trmttm <96355189+trmttm@users.noreply.github.com>
2022-09-02 09:26:17 +0400

    bug fix for Sabolyman in Windows

--- Controller/keyboard.py
+++ Controller/keyboard.py
@@ -1,10 +1,11 @@
+import WidgetNames
+import os_identifier
+from Entities import EntitiesABC
+from Interactor import InteractorABC
 from interface_keymaps import KeyMapsABC
 from interface_view import ViewABC
 from keyboard_shortcut import KeyMap
 
-import WidgetNames
-from Entities import EntitiesABC
-from Interactor import InteractorABC
 from . import state
 from . import utilities
 
@@ -13,23 +14,30 @@ def configure_keyboard_shortcut(app: ViewABC, i: InteractorABC, e: EntitiesABC):
     i.set_active_keymap('default')
     f = i.add_new_keyboard_shortcut
 
-    f((KeyMap.command, KeyMap.l), (lambda: i.load_state_from_file(app.select_open_file()), ''))
-    f((KeyMap.command, KeyMap.s), (lambda: i.save_state(), ''))
-    f((KeyMap.command + KeyMap.shift, KeyMap.s), (lambda: i.save_to_file(utilities.default_file_path(i, e)), ''))
-    f((KeyMap.command, KeyMap.w), (lambda: i.close(lambda: app.close('root')), ''))
-    f((KeyMap.command, KeyMap.d), (lambda: i.duplicate_selected_card(), ''))
-    f((KeyMap.command, KeyMap.m), (lambda: i.make_email(), ''))
-    f((KeyMap.control, KeyMap.c), (lambda: i.set_color_to_cards(state.get_left_tree_selected_indexes(app),
-                                                                state.get_right_tree_selected_indexes(app),
-                                                                app.ask_color()), ''))
-    f((KeyMap.control, KeyMap.h), (lambda: i.toggle_hide_finished_cards(), ''))
-    f((KeyMap.command, KeyMap.f), (lambda: app.focus(WidgetNames.entry_search_box), ''))
+    if os_identifier.is_mac:
+        main_modifier = KeyMap.command
+        sub_modifier = KeyMap.control
+    else:
+        main_modifier = KeyMap.control
+        sub_modifier = KeyMap.alt_option
 
-    f((KeyMap.command, KeyMap.one), (lambda: i.sort_cards_by_color(), ''))
-    f((KeyMap.command, KeyMap.two), (lambda: i.sort_cards_by_deadline(), ''))
-    f((KeyMap.command, KeyMap.three), (lambda: i.sort_cards_by_name(), ''))
-    f((KeyMap.command, KeyMap.four), (lambda: i.sort_cards_by_current_owner(), ''))
-    f((KeyMap.command, KeyMap.five), (lambda: i.sort_cards_by_current_client(), ''))
+    f((main_modifier, KeyMap.l), (lambda: i.load_state_from_file(app.select_open_file()), ''))
+    f((main_modifier, KeyMap.s), (lambda: i.save_state(), ''))
+    f((main_modifier + KeyMap.shift, KeyMap.s), (lambda: i.save_to_file(utilities.default_file_path(i, e)), ''))
+    f((main_modifier, KeyMap.w), (lambda: i.close(lambda: app.close('root')), ''))
+    f((main_modifier, KeyMap.d), (lambda: i.duplicate_selected_card(), ''))
+    f((main_modifier, KeyMap.m), (lambda: i.make_email(), ''))
+    f((sub_modifier, KeyMap.c), (lambda: i.set_color_to_cards(state.get_left_tree_selected_indexes(app),
+                                                              state.get_right_tree_selected_indexes(app),
+                                                              app.ask_color()), ''))
+    f((sub_modifier, KeyMap.h), (lambda: i.toggle_hide_finished_cards(), ''))
+    f((main_modifier, KeyMap.f), (lambda: app.focus(WidgetNames.entry_search_box), ''))
+
+    f((main_modifier, KeyMap.one), (lambda: i.sort_cards_by_color(), ''))
+    f((main_modifier, KeyMap.two), (lambda: i.sort_cards_by_deadline(), ''))
+    f((main_modifier, KeyMap.three), (lambda: i.sort_cards_by_name(), ''))
+    f((main_modifier, KeyMap.four), (lambda: i.sort_cards_by_current_owner(), ''))
+    f((main_modifier, KeyMap.five), (lambda: i.sort_cards_by_current_client(), ''))
 
     # i.set_active_keymap('special')
     # f((KeyMap.command, KeyMap.a), (lambda: print('Hello!'), ''))
