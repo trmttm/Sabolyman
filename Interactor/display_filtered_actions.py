3a2709f0959042307dc5005f603e16955acea353
trmttm <96355189+trmttm@users.noreply.github.com>
2022-12-14 19:13:59 +0400

    Improved text display of list of cards and actions.

--- Interactor/display_filtered_actions.py
+++ Interactor/display_filtered_actions.py
@@ -5,12 +5,24 @@ from Entities import EntitiesABC
 
 def execute(title: str, filter_action: Callable, feed_back: Callable, e: EntitiesABC):
     text_to_display = ''
+    counter_card = 0
+    counter_all_actions = 0
     for card in e.all_cards:
         card_text = ''
+        counter_action = 0
         for action in card.actions.all_actions:
             if filter_action(action):
-                card_text += f'{action.name}\n'
+                card_text += f'\t{counter_action + 1}\t{action.name}\n'
+                counter_action += 1
+                counter_all_actions += 1
+
         if card_text != '':
-            text_to_display += f'\n\n[{card.name}]\n'
+            if counter_card > 0:
+                text_to_display += '\n\n'
+            text_to_display += f'【{counter_card + 1} {card.name}】\n'
+            counter_card += 1
+
             text_to_display += card_text
-    feed_back(title, text_to_display, 400, 600, by_textbox=True)
+
+    prefix = f'Total {counter_card} cards, {counter_all_actions} actions\n\n'
+    feed_back(title, prefix + text_to_display, 800, 800, by_textbox=True)
