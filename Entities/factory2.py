d0235a42f7d916748360b771c2c30fb0eb9575c0
trmttm <96355189+trmttm@users.noreply.github.com>
2023-01-28 09:13:23 +0400

    Update factory2.py

--- Entities/factory2.py
+++ Entities/factory2.py
@@ -3,6 +3,8 @@ from .actions import Actions
 
 # Can NOT be depended by Action
 def factory_actions(state: dict, alias_actions_dictionary: dict = None) -> Actions:
+    if alias_actions_dictionary is None:
+        alias_actions_dictionary = {}
     actions = Actions()
     actions_state = state.get('actions', {})
     actions.load_state(actions_state, alias_actions_dictionary)
