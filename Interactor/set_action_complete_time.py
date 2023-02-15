6f3d776579795bc02d64e9458e86cff90d3080b6
trmttm <96355189+trmttm@users.noreply.github.com>
2023-02-15 17:53:22 +0400

    Update set_action_complete_time.py

--- Interactor/set_action_complete_time.py
+++ Interactor/set_action_complete_time.py
@@ -9,8 +9,8 @@ def execute(e: EntitiesABC, done_or_not, actions_indexes: Tuple[int, ...] = None
         actions_indexes = (e.active_action_index,)
     for i, action in enumerate(e.active_card.all_actions):
         if i in actions_indexes:
-            if done_or_not and action.is_done:  # note that action mark done may be prevented if its policy action
-                if action.time_completed is None:
-                    action.set_completed_time(datetime.datetime.now())
-            if not done_or_not and not action.is_done:  # just cuz done_or_not is False does not always set_imcomplete
+            if done_or_not and action.is_done and action.time_completed is None:
+                # note that action mark done may be prevented if its policy action
+                action.set_completed_time(datetime.datetime.now())
+            if not done_or_not and not action.is_done:  # just cuz done_or_not is False does not always set_incomplete
                 action.set_incomplete()
