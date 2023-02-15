3a55eb100981a88e88b7f269629b7a6e32e0cb14
trmttm <96355189+trmttm@users.noreply.github.com>
2023-02-15 16:18:53 +0400

    Update jump_to_card.py

--- Interactor/jump_to_card.py
+++ Interactor/jump_to_card.py
@@ -9,8 +9,16 @@ def execute(card_: Card, e: EntitiesABC, p: PresentersABC):
     if card_ is not None:
         e.set_active_card(card_)
         indexes = (e.active_card_index,)
-        if indexes is not None:
+        if indexes is not None and card_to_jump_to_is_visible(card_, e, indexes):
             if e.active_card_is_in_my_cards:
                 show_my_card_information.execute(e, p, indexes)
             else:
                 show_their_card_information.execute(e, p, indexes)
+
+
+def card_to_jump_to_is_visible(card_, e, indexes):
+    if e.active_card_is_in_my_cards:
+        card_is_visible = (e.get_my_card_by_index(indexes[0]) is card_)
+    else:
+        card_is_visible = (e.get_their_card_by_index(indexes[0]) is card_)
+    return card_is_visible
