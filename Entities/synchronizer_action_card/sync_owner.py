54538de9b3324975415b8432af5add890fa12a06
trmttm <96355189+trmttm@users.noreply.github.com>
2023-02-21 18:54:25 +0400

    Fix bug

--- Entities/synchronizer_action_card/sync_owner.py
+++ Entities/synchronizer_action_card/sync_owner.py
@@ -5,12 +5,11 @@ from Entities import Card
 def sync_owner(policy_action: Action, implementation_card: Card):
     def wrapper(a: Action):
         def wrapped():
-            current_owner = None
-            for action_in_imple_card in implementation_card.all_actions:
-                if not action_in_imple_card.is_done:
-                    current_owner = action_in_imple_card.get_owner()
+            for action_in_impl_card in implementation_card.all_actions:
+                if not action_in_impl_card.is_done:
+                    current_owner = action_in_impl_card.get_owner()
+                    a.set_owner(current_owner)
                     break
-            a.set_owner(current_owner)
             return a.unwrapped_get_owner()
 
         return wrapped
