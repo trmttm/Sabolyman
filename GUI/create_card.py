2c51117d7e0719ef6aa076d985c1d8f1d3fff8ac
trmttm <96355189+trmttm@users.noreply.github.com>
2023-02-25 22:03:04 +0400

    Add Action start / end in GUI

--- GUI/create_card.py
+++ GUI/create_card.py
@@ -60,6 +60,21 @@ def get_actions_properties(stacker: Stacker):
         stacker.hstack(
             w.Label('lbl_action_deadline').text('Dead line:').width(10).padding(5, 0),
             w.Entry(wn.entry_action_dead_line).default_value('2022/07/22 15:00').padding(25, 0),
+            w.Button(wn.button_set_deadline).text('+').width(1).padding(25, 0),
+            w.Spacer().adjust(-2),
+        ),
+        stacker.hstack(
+            w.Label('lbl_action_start').text('Start from:').width(10).padding(5, 0),
+            w.Entry(wn.entry_action_start_from).default_value('2022/07/22 15:00').padding(25, 0),
+            w.Button(wn.button_set_start_from).text('+').width(1).padding(25, 0),
+            w.Spacer().adjust(-2),
+        ),
+        stacker.hstack(
+            w.Label('lbl_time').text('Time expected').width(10).padding(5, 0),
+            stacker.hstack(
+                w.Entry(wn.entry_action_time_expected).default_value('0:00').padding(25, 0).width(10),
+                w.Button(wn.button_set_duration).text('+').width(1).padding(25, 0),
+            ),
             w.Spacer().adjust(-1),
         ),
         stacker.hstack(
@@ -71,14 +86,6 @@ def get_actions_properties(stacker: Stacker):
             w.Label('lbl_client').text('Client').width(10).padding(5, 0),
             w.Entry(wn.entry_action_client).default_value('Client Name').padding(25, 0),
             w.Spacer().adjust(-1),
-        ),
-        stacker.hstack(
-            w.Label('lbl_time').text('Time expected').width(10).padding(5, 0),
-            stacker.hstack(
-                w.Entry(wn.entry_action_time_expected).default_value('0:00').padding(25, 0).width(10),
-                w.Button(wn.button_set_duration).text('+').width(1),
-            ),
-            w.Spacer(),
         ),
         stacker.hstack(
             w.Label('lbl_done').text('Done').width(10).padding(5, 0),
