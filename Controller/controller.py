e36c4f22844b169b8b639ebf0ca272c1b86f4d04
trmttm <96355189+trmttm@users.noreply.github.com>
2023-02-26 15:09:34 +0400

    Remove unintended comma

--- Controller/controller.py
+++ Controller/controller.py
@@ -73,7 +73,7 @@ def configure_controller(v: ViewABC, i: InteractorABC):
     f(wn.entry_action_dead_line, lambda *_: wrapper(lambda *_: i.set_dead_line(s.get_dead_line_str(v), ai(v))))
     f(wn.entry_action_start_from, lambda *_: wrapper(lambda *_: i.set_start_from(s.get_start_from_str(v), ai(v))))
     f(wn.check_button_action_done, lambda *_: i.mark_action_completed(s.get_action_is_done_or_not(v), ai(v)))
-    f(wn.text_box_action_description, lambda *_: i.set_action_description(s.get_action_description(v), ai(v))),
+    f(wn.text_box_action_description, lambda *_: i.set_action_description(s.get_action_description(v), ai(v)))
 
 
 def upon_action_tree_entrance(v: ViewABC):
