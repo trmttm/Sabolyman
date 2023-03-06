15de387f8ee44b5ec009e766a75b98f4fba90d77
trmttm <96355189+trmttm@users.noreply.github.com>
2023-03-06 06:08:52 +0400

    Duplicate card properly clones implementation structures

--- Commands/duplicate_card.py
+++ Commands/duplicate_card.py
@@ -9,17 +9,21 @@ class DuplicateCard(UseCase):
         self._entities = entities
         self._original_card = original_card
 
-    def execute(self):
+    def execute(self) -> Card:
         clone_card = Card()
         clone_card.load_state(self._original_card.state)
         self._entities.force_set_ids(clone_card)
 
         s: SynchronizerABC = self._entities.synchronizer
-        for a in clone_card.all_actions:
-            if s.action_has_implementation_card(a.id):
-                DuplicateCard(self._entities, s.get_implementation_card(a.id)).execute()  # Recursively clone
+        for clone_action, original_action in zip(clone_card.all_actions, self._original_card.all_actions):
+            if s.action_has_implementation_card(original_action.id):
+                # Recursively clone
+                sub_clone_card = DuplicateCard(self._entities, s.get_implementation_card(original_action.id)).execute()
+                self._entities.synchronize_action_to_card(clone_action, sub_clone_card)
 
         clone_card.set_name(f'{clone_card.name} copied')
         clone_card.update_date_created()
         self._entities.add_new_card(clone_card)
         self._entities.set_active_card(clone_card)
+
+        return clone_card
