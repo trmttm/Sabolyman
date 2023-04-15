b1883e3cc63a6d9ebad31d9620730ad7b6189a61
trmttm <96355189+trmttm@users.noreply.github.com>
2023-04-15 11:10:05 +0900

    Abstracted action pasting it's original card is now prevented.

--- Interactor/paste_actions_as_alias.py
+++ Interactor/paste_actions_as_alias.py
@@ -9,25 +9,37 @@ from . import present_action_list
 
 
 def execute(e: EntitiesABC, p: PresentersABC, feedback_method: Callable):
-    duplicate_actions, n_new_actions = create_alias_of_action_if_not_duplicate(e)
+    feedback, n_new_actions = create_alias_of_action_if_not_duplicate_or_incursion(e)
     handle_done_not_done_status_and_select_the_right_card(e)
     remove_actions_if_cut_mode(e)
     present_actions(e, p, n_new_actions)
-    feedback_user_if_duplicate(duplicate_actions, feedback_method)
+    feedback_user_if_duplicate_or_incursion(feedback, feedback_method)
 
 
-def create_alias_of_action_if_not_duplicate(e: EntitiesABC):
+def create_alias_of_action_if_not_duplicate_or_incursion(e: EntitiesABC):
     duplicate_actions = []
+    copying_to_itself = []
+    feedback = {'duplicate_actions': duplicate_actions, 'copying_to_itself': copying_to_itself, }
     n_new_actions = 0
 
     for action in e.copied_actions:
-        if action not in e.active_card.all_actions:
+        if action in e.active_card.all_actions:
+            duplicate_actions.append(action)
+        elif check_if_pasting_to_itself(action, e):
+            copying_to_itself.append(action)
+        else:
             action.set_id()
             e.add_new_action(action)
             n_new_actions += 1
-        else:
-            duplicate_actions.append(action)
-    return duplicate_actions, n_new_actions
+    return feedback, n_new_actions
+
+
+def check_if_pasting_to_itself(action: Action, e: EntitiesABC) -> bool:
+    implementation_card = e.synchronizer.get_implementation_card(action.id)
+    if implementation_card is not None:
+        return implementation_card.id == e.active_card.id
+    else:
+        return False
 
 
 def remove_actions_if_cut_mode(e: EntitiesABC):
@@ -65,10 +77,20 @@ def present_actions(e: EntitiesABC, p: PresentersABC, last_n_actions: int):
         present_action_list.execute(e, p, next_selection_index)
 
 
-def feedback_user_if_duplicate(actions_that_already_exist_in_active_card, feedback_method):
+def feedback_user_if_duplicate_or_incursion(feedback, feedback_method):
+    actions_that_already_exist_in_active_card = feedback.get('duplicate_actions', [])
+    body_text = 'The following alias(es) not created as the actions already exist.'
+    feedback_user(actions_that_already_exist_in_active_card, body_text, feedback_method)
+
+    actions_that_is_copying_to_itself = feedback.get('copying_to_itself', [])
+    body_text = 'The following alias(es) is tyring to paste to itself.'
+    feedback_user(actions_that_is_copying_to_itself, body_text, feedback_method)
+
+
+def feedback_user(actions_that_already_exist_in_active_card, body_text, feedback_method):
     if actions_that_already_exist_in_active_card:
         title = 'Actions already exist'
-        body = 'The following alias(es) not created as the actions already exist.\n'
+        body = '%s\n' % body_text
         for n, action in enumerate(actions_that_already_exist_in_active_card):
             body += f'  {n + 1} {action.name}\n'
         feedback_method(title, body, 600, 200)
