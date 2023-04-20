f8d98eacfa43804d5404f4542e82714935274775
trmttm <96355189+trmttm@users.noreply.github.com>
2023-04-20 15:22:24 +0900

    Refactored and extracted keyboard shortcut setting

--- sabolyman/main.py
+++ sabolyman/main.py
@@ -1,11 +1,9 @@
-import gui
-from interface_tk import top_level_options
-from interface_tk import widget_model
 from view_tkinter import View
 
 import Controller.controller
 import Controller.keyboard
 import Controller.keyboard_config_file
+import Controller.keyboard_shortcut_menu
 import Controller.menu_bar
 from Entities import Entities
 from Gateway import Gateway
@@ -29,52 +27,13 @@ def start_app(gui_selected='gui01.gui'):
     # Widget command mapping
     Controller.controller.configure_controller(app, interactor)
     # Menu bar
-    f = app.select_folder
-    j = Controller.menu_bar.configure_menu_bar
 
-    file_path = Controller.keyboard_config_file.get_file_path(interactor)
-    command_names = Controller.keyboard_config_file.get_method_name_to_key_combo(app, interactor, entities).keys()
-    n_commands = len(command_names)
+    decorate_setting_menu = Controller.keyboard_shortcut_menu.create_setting_menu(app, entities, interactor)
 
-    specified_parent = 'pop_up_shortcut_setting'
-    title = 'Filter setting'
-    width = 1000
-    height = 500
-    options = top_level_options(title, (width, height))
+    def callback_configure_menu(menu_injected: dict):
+        Controller.menu_bar.configure_menu_bar(app, interactor, entities, decorate_setting_menu(menu_injected))
 
-    def get_view_model_shortcut_setting():
-        view_model = [widget_model('root', specified_parent, 'toplevel', 0, 0, 0, 0, 'nswe', **options)]
-        commands_to_short_cuts = Controller.keyboard_config_file.get_method_name_to_key_combo(app, interactor, entities)
-        return view_model + gui.create_view_model_of_shortcut_setter(callback, commands_to_short_cuts, specified_parent)
-
-    def save_commands():
-        data = dict(zip(command_names, gui.get_state(app, n_commands)))
-        gui.save_shortcut_configuration_file(file_path, data)
-        Controller.keyboard_config_file.configure_keyboard_shortcut(app, interactor, entities)
-
-    def callback(command_str):
-        print(command_str, gui.get_state(app, n_commands))
-        if command_str == gui.KEY_CANCEL:
-            app.close(specified_parent)
-        elif command_str == gui.KEY_APPLY:
-            save_commands()
-        elif command_str == gui.KEY_DONE:
-            save_commands()
-            app.close(specified_parent)
-
-    def menu_wrapper(menu_data: dict):
-        def open_keyboard_shortcut_setting():
-            app.add_widgets(get_view_model_shortcut_setting())
-            [gui.bind_commands(n, app) for n in range(n_commands)]
-
-        injected_menu = {
-            'Setting': {
-                'Keyboard shortcut': lambda: open_keyboard_shortcut_setting(),
-            }, }
-        menu_data.update(injected_menu)
-        return menu_data
-
-    interactor.create_mail_menu(f, lambda menu_injected: j(app, interactor, entities, menu_wrapper(menu_injected)))
+    interactor.create_mail_menu(app.select_folder, callback_configure_menu)
     # Keyboard shortcut
     configure_shortcut_by_file = True
     if configure_shortcut_by_file:
