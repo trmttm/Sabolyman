31e254d232e185037b4a21b1fcf8297d4465ae71
trmttm <96355189+trmttm@users.noreply.github.com>
2023-04-24 07:34:24 +0900

    Ctrl + 8 should only show the lowest level actions.

--- Interactor/display_progress.py
+++ Interactor/display_progress.py
@@ -3,6 +3,7 @@ from typing import Callable
 
 from Entities import Action
 from Entities import EntitiesABC
+from Entities.synchronizer_action_card import SynchronizerABC
 from . import display_filtered_actions
 
 text_color = 'dark green'
@@ -12,10 +13,11 @@ def execute(from_: str, to_: str, feedback: Callable, e: EntitiesABC):
     title = f'Progress from {from_} to {to_}...'
     date_from = datetime.datetime.strptime(from_, '%Y/%m/%d').date()
     date_to = datetime.datetime.strptime(to_, '%Y/%m/%d').date()
+    s: SynchronizerABC = e.synchronizer
 
     def filter_action(action: Action):
         datetime_in_question = action.time_completed
-        if datetime_in_question is not None:
+        if (datetime_in_question is not None) and (not s.action_has_implementation_card(action.id)):
             date = datetime_in_question.date()
             if (date_from <= date) and (date <= date_to):
                 return True
