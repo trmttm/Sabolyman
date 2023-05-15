56096817adb20f3850cff55ea2c263aa69275bd0
trmttm <96355189+trmttm@users.noreply.github.com>
2023-05-16 07:30:06 +0900

    Update sync_mark_done.py

--- Entities/synchronizer_action_card/sync_mark_done.py
+++ Entities/synchronizer_action_card/sync_mark_done.py
@@ -6,18 +6,19 @@ from Entities.card import Card
 from .abc import SynchronizerABC
 
 
-def sync_mark_done(e: EntitiesABC, implementation_card: Card, get_policy_action: Callable[[str], Action]):
+def sync_mark_done(e: EntitiesABC, active_card: Card, get_policy_action: Callable[[str], Action]):
     synchronizer: SynchronizerABC = e.synchronizer
-    for action in implementation_card.all_actions:
-        def wrapper_mark_done(mark_done: Callable):
+    for action in active_card.all_actions:
+        def wrapper_mark_done(action_passed: Action):
             def wrapped():
-                mark_done()
-                if implementation_card.is_done:
-                    policy_action = get_policy_action(implementation_card.id)
-                    if policy_action is not None:
-                        policy_action.mark_done_programmatically()
-                        parent_cards = synchronizer.get_immediate_parents(e.active_card)
-                        if len(parent_cards) > 0:
+                action_passed.mark_done_programmatically()
+                parents = e.get_cards_that_have_action(action_passed)
+                for parent_card in parents:
+                    if parent_card.is_done:
+                        policy_action = get_policy_action(parent_card.id)  # This has to happen recursively
+                        if policy_action is not None:
+                            policy_action.mark_done_programmatically()
+                            parent_cards = synchronizer.get_immediate_parents(e.active_card)
                             e.set_active_card(parent_cards[0])
 
             return wrapped
@@ -25,13 +26,13 @@ def sync_mark_done(e: EntitiesABC, implementation_card: Card, get_policy_action:
         def wrapper_mark_not_done(mark_not_done: Callable):
             def wrapped():
                 mark_not_done()
-                policy_action = get_policy_action(implementation_card.id)
+                policy_action = get_policy_action(active_card.id)
                 if policy_action is not None:
                     policy_action.mark_not_done_programmatically()
 
             return wrapped
 
-        action.mark_done = wrapper_mark_done(action.mark_done)
+        action.mark_done = wrapper_mark_done(action)
         action.mark_not_done = wrapper_mark_not_done(action.mark_not_done)
 
 
