78621e0d79428c5b4b11c42dff610c0e58f331a0
trmttm <96355189+trmttm@users.noreply.github.com>
2023-05-20 00:42:15 +0900

    Create GUI list of all actions

--- Tests/tests_gui.py
+++ Tests/tests_gui.py
@@ -26,6 +26,72 @@ def load_gui_from_pickle(file_name, specified_parent, parent_widget='toplevel'):
     return view
 
 
+def create_data():
+    import datetime
+    import Utilities
+    import GUI.list_of_actions as c
+    data = {
+        c.KEY_DATE: datetime.datetime(2023, 5, 15),
+        c.KEY_CARD_STATES: (
+            {
+                c.CARD_NAME: 'Card 01',
+                c.KEY_ACTION_IDS: (
+                    'id0101',
+                    'id0102',
+                    'id0103',
+                    'id0104',
+                ),
+                c.KEY_NAMES: (
+                    'action 0101',
+                    'action 0102',
+                    'action 0103',
+                    'action 0104',
+                ),
+                c.KEY_DONE_OR_NOT: (
+                    False,
+                    False,
+                    False,
+                    False,
+                ),
+                c.KEY_DUE_DATES: (
+                    Utilities.datetime_to_str_no_time(Utilities.str_to_date_time_no_time('2023/5/12')),
+                    Utilities.datetime_to_str_no_time(Utilities.str_to_date_time_no_time('2023/5/13')),
+                    Utilities.datetime_to_str_no_time(Utilities.str_to_date_time_no_time('2023/5/14')),
+                    Utilities.datetime_to_str_no_time(Utilities.str_to_date_time_no_time('2023/5/15')),
+                ),
+            },
+            {
+                c.CARD_NAME: 'Card 02',
+                c.KEY_ACTION_IDS: (
+                    'id0201',
+                    'id0202',
+                    'id0203',
+                    'id0204',
+                ),
+                c.KEY_NAMES: (
+                    'action 0201',
+                    'action 0202',
+                    'action 0203',
+                    'action 0204',
+                ),
+                c.KEY_DONE_OR_NOT: (
+                    False,
+                    False,
+                    False,
+                    False,
+                ),
+                c.KEY_DUE_DATES: (
+                    Utilities.datetime_to_str_no_time(Utilities.str_to_date_time_no_time('2023/5/15')),
+                    Utilities.datetime_to_str_no_time(Utilities.str_to_date_time_no_time('2023/5/15')),
+                    Utilities.datetime_to_str_no_time(Utilities.str_to_date_time_no_time('2023/5/15')),
+                    Utilities.datetime_to_str_no_time(Utilities.str_to_date_time_no_time('2023/5/15')),
+                ),
+            },
+        )
+    }
+    return data
+
+
 class MyTestCase(unittest.TestCase):
     def test_list_of_balls(self):
         import GUI
@@ -196,6 +262,28 @@ class MyTestCase(unittest.TestCase):
         from view_tkinter import View
         view = View(fullscreen=True)
         view.add_widgets(view_model)
+        view.launch_app()
+
+    def test_list_of_actions(self):
+        import GUI
+        data = create_data()
+
+        from interface_tk import widget_model as wm
+        from interface_tk import top_level_options
+        options = top_level_options('List of Acgtions', (600, 400))
+        view_model = [wm('root', GUI.list_of_actions.POPUP, 'toplevel', 0, 0, 0, 0, 'nswe', **options)]
+        view_model += GUI.list_of_actions.get_view_model(GUI.list_of_actions.POPUP, data)
+
+        from view_tkinter import View
+        view = View()
+        view.add_widgets(view_model)
+
+        def callback(state: tuple):
+            print()
+            for action_state in state:
+                print(action_state)
+
+        GUI.list_of_actions.bind_commands(view, callback, data)
         view.launch_app()
 
 
