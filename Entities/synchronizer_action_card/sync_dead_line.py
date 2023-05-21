77759e6a710406952a078849c5b4bc11a5549e79
trmttm <96355189+trmttm@users.noreply.github.com>
2023-05-22 04:47:08 +0900

    Update sync_dead_line.py

--- Entities/synchronizer_action_card/sync_dead_line.py
+++ Entities/synchronizer_action_card/sync_dead_line.py
@@ -20,7 +20,8 @@ def sync_dead_line(policy_action: Action, get_implementation_card: Callable[[str
 
     def wrapper_set(action: Action):
         def wrapped(*args, **kwargs):
-            set_action_dead_line_recursively(action, get_implementation_card, e, s, args, kwargs)
+            actions_already_handled = {action.id}
+            set_dead_line_recursively(action, get_implementation_card, e, s, args, kwargs, actions_already_handled)
 
         return wrapped
 
@@ -41,18 +42,20 @@ def policy_has_already_been_wrapped(a: Action):
         return False
 
 
-def set_action_dead_line_recursively(action: Action, get_implementation_card: Callable, e: EntitiesABC,
-                                     s: SynchronizerABC, args, kwargs):
-    implementation_card = get_implementation_card(action.id)
-    if implementation_card is not None:
-        current_dead_line = action.get_dead_line_programmatically()
-        new_dead_line = args[0]
-        days = (new_dead_line - current_dead_line).days
-        implementation_card.increment_deadline_by(days)
-    action.set_dead_line_programmatically(*args, **kwargs)
-    cards = e.get_cards_that_have_action(action)
-    for card in cards:
-        parent_cards = s.get_immediate_parents(card)
-        for each_parent_card in parent_cards:
-            ancestor_action = s.card_has_policy_action(each_parent_card.id)
-            set_action_dead_line_recursively(ancestor_action, get_implementation_card, e, s, args, kwargs)
+def set_dead_line_recursively(action: Action, get_implementation_card: Callable, e: EntitiesABC,
+                              s: SynchronizerABC, args, kwargs, already_handled: set):
+    already_handled.add(action.id)
+    if action.id not in already_handled:
+        implementation_card = get_implementation_card(action.id)
+        if implementation_card is not None:
+            current_dead_line = action.get_dead_line_programmatically()
+            new_dead_line = args[0]
+            days = (new_dead_line - current_dead_line).days
+            implementation_card.increment_deadline_by(days)
+        action.set_dead_line_programmatically(*args, **kwargs)
+        cards = e.get_cards_that_have_action(action)
+        for card in cards:
+            parent_cards = s.get_immediate_parents(card)
+            for each_parent_card in parent_cards:
+                ancestor_action = s.get_policy_action(each_parent_card.id)
+                set_dead_line_recursively(ancestor_action, get_implementation_card, e, s, args, kwargs, already_handled)
