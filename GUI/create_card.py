4e37515afdaa65dca4faf7c47c494d20d738b73c
trmttm <96355189+trmttm@users.noreply.github.com>
2023-05-26 04:34:06 +0900

    New GUI includes Action Scheduled state

--- GUI/create_card.py
+++ GUI/create_card.py
@@ -75,6 +75,18 @@ def get_actions_properties(stacker: Stacker):
                     w.Spacer().adjust(-2),
                 ),
                 stacker.hstack(
+                    w.Label('lbl_owner').text('Owner').width(10).padding(5, 0),
+                    w.Entry(wn.entry_action_owner).default_value('Owner Name').padding(25, 0),
+                    w.Button(wn.button_set_owner).text('+').width(1).padding(25, 0),
+                    w.Spacer().adjust(-2),
+                ),
+                stacker.hstack(
+                    w.Label('lbl_client').text('Client').width(10).padding(5, 0),
+                    w.Entry(wn.entry_action_client).default_value('Client Name').padding(25, 0),
+                    w.Button(wn.button_set_client).text('+').width(1).padding(25, 0),
+                    w.Spacer().adjust(-2),
+                ),
+                stacker.hstack(
                     w.Label('lbl_time').text('Time expected').width(10).padding(5, 0),
                     stacker.hstack(
                         w.Entry(wn.entry_action_time_expected).default_value('0:00').padding(25, 0).width(10),
@@ -83,14 +95,9 @@ def get_actions_properties(stacker: Stacker):
                     w.Spacer().adjust(-1),
                 ),
                 stacker.hstack(
-                    w.Label('lbl_owner').text('Owner').width(10).padding(5, 0),
-                    w.Entry(wn.entry_action_owner).default_value('Owner Name').padding(25, 0),
-                    w.Spacer().adjust(-1),
-                ),
-                stacker.hstack(
-                    w.Label('lbl_client').text('Client').width(10).padding(5, 0),
-                    w.Entry(wn.entry_action_client).default_value('Client Name').padding(25, 0),
-                    w.Spacer().adjust(-1),
+                    w.Label('lbl_scheduled').text('Scheduled').width(10).padding(5, 0),
+                    w.CheckButton(wn.check_button_action_scheduled).value(False).padding(25, 0),
+                    w.Spacer().adjust(-2),
                 ),
                 stacker.hstack(
                     w.Label('lbl_done').text('Done').width(10).padding(5, 0),
