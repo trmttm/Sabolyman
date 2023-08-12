bb2da05273929b7a7b0ee888fb335357b54aa618
trmttm <96355189+trmttm@users.noreply.github.com>
2023-08-12 23:06:12 +0900

    New GUI

--- Tests/tests_gui.py
+++ Tests/tests_gui.py
@@ -264,6 +264,33 @@ class MyTestCase(unittest.TestCase):
         view.add_widgets(view_model)
         view.launch_app()
 
+    def test_combined_05(self):
+        save_pickle = True
+
+        import GUI
+        from stacker import Stacker
+        from stacker import widgets as w
+        stacker = Stacker()
+        stacker.vstack(
+            w.PanedWindow('pw_main', stacker).weights((1, 0)).is_horizontal().stackers(
+                w.Label('1').text(''),
+                w.Label('2').text('')
+            )
+        )
+        view_model = stacker.view_model
+        view_model += GUI.list_of_balls.get_view_model('frame_1')
+        view_model += GUI.create_card.get_view_model('frame_2', vertical=True)
+
+        if save_pickle:
+            import pickle
+            with open('gui05.gui', 'wb') as f:
+                pickle.dump(view_model, f)
+
+        from view_tkinter import View
+        view = View(fullscreen=True)
+        view.add_widgets(view_model)
+        view.launch_app()
+
     def test_list_of_actions(self):
 
         from view_tkinter import View
